b05bcfcca5a8e1963884e92cd0ef0229adca7fd2
Alfred Faith <111481054+TonyFred-code@users.noreply.github.com>
2026-01-20 21:11:06 +0100

    feat: add index router handler

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 import express from "express";
 import { dirname, join } from "path";
 import { fileURLToPath } from "url";
+import { indexRouter } from "./routes/indexRouter.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
+
 app.set("views", join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/", (req, res) => {
-  res.send("Hello world");
-});
+app.use("/", indexRouter);
 
 const PORT = process.env.PORT || 8000;
 
