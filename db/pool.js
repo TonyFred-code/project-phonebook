ecf35676fa0a2c3fdeee4b97ac621dd65fe350e5
Alfred Faith <111481054+TonyFred-code@users.noreply.github.com>
2026-01-21 14:40:54 +0100

    feat: create database connection pool export instance

---
+++ db/pool.js
@@ -0,0 +1,11 @@
+import { Pool } from "pg";
+import { configDotenv } from "dotenv";
+
+configDotenv();
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
+});
+
+export default pool;
