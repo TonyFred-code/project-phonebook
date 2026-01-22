caa04a786ae0de0bb66b03bc2adf2d2696209424
Alfred Faith <111481054+TonyFred-code@users.noreply.github.com>
2026-01-22 09:05:42 +0100

    feat: add getAllContacts controller function
    render all contacts in database

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,5 +1,11 @@
+import { getAllContacts } from "../db/queries.js";
+
 async function getPhoneBook(req, res) {
-  res.render("index");
+  const contacts = await getAllContacts();
+
+  console.log("Contacts: ", contacts);
+
+  res.render("index", { contacts });
 }
 
 export { getPhoneBook };
