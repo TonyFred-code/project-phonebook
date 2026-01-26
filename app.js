163368588b21bbee41ae50879da830f54734c14e
Alfred Faith <111481054+TonyFred-code@users.noreply.github.com>
2026-01-26 23:47:16 +0100

    refactor: update app error middleware
    log full error to the server console
    update error view page title

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
 
 // **Error-handling middleware**
 app.use((err, req, res, next) => {
-  console.error(err.stack);
+  console.error(err);
 
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
