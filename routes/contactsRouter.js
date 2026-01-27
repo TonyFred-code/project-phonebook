c16557155a235950639754f8d2e6b185de01c30d
Alfred Faith <111481054+TonyFred-code@users.noreply.github.com>
2026-01-27 01:18:36 +0100

    feat: add contact edit router get path

--- routes/contactsRouter.js
+++ routes/contactsRouter.js
@@ -4,11 +4,13 @@ import {
   createContactGet,
   createContactPost,
   getPhoneBook,
+  updateContactGet,
 } from "../controllers/contactsController.js";
 
 const contactsRouter = Router();
 
 contactsRouter.get("/new", createContactGet);
+contactsRouter.get("/:contactId/edit", updateContactGet);
 contactsRouter.get("/:contactId", contactById);
 contactsRouter.post("/", createContactPost);
 contactsRouter.get("/", getPhoneBook);
