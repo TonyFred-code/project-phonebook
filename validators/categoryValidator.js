483d853c7b6bd1876d28b3915d458096e05c3af0
Alfred Faith <111481054+TonyFred-code@users.noreply.github.com>
2026-01-29 20:12:34 +0100

    feat: create validator rules for category
    create validator rules for creating and updating category data

---
+++ validators/categoryValidator.js
@@ -0,0 +1,24 @@
+import { body } from "express-validator";
+
+function categoryValidationRules() {
+  return [
+    body("category_name")
+      .trim()
+      .notEmpty()
+      .withMessage("Category name is required")
+      .isAlphanumeric("en-US", { ignore: "_-" })
+      .withMessage(
+        "Username can only contain letters, numbers, underscores and hyphens"
+      )
+      .isLength({ min: 1, max: 100 })
+      .withMessage("Category name must be between 1 and 100 characters"),
+
+    body("category_description")
+      .optional({ checkFalsy: true })
+      .trim()
+      .isLength({ max: 255 })
+      .withMessage("Category description must not exceed 255 characters"),
+  ];
+}
+
+export { categoryValidationRules };
