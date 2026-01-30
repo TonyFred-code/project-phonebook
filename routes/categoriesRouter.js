5e1c87957b8afea777745a977805a70ccfeab834
Alfred Faith <111481054+TonyFred-code@users.noreply.github.com>
2026-01-30 01:56:35 +0100

    feat: add router for edit category post path

--- routes/categoriesRouter.js
+++ routes/categoriesRouter.js
@@ -5,6 +5,7 @@ import {
   getAllCategories,
   getCategory,
   updateCategoryGet,
+  updateCategoryPost,
 } from "../controllers/categoriesController.js";
 import { categoryValidationRules } from "../validators/categoryValidator.js";
 
@@ -12,6 +13,11 @@ const categoriesRouter = Router();
 
 categoriesRouter.post("/new", categoryValidationRules(), createCategoryPost);
 categoriesRouter.get("/new", createCategoryGet);
+categoriesRouter.post(
+  "/:categoryId/update",
+  categoryValidationRules(),
+  updateCategoryPost
+);
 categoriesRouter.get("/:categoryId/edit", updateCategoryGet);
 categoriesRouter.get("/:categoryId", getCategory);
 categoriesRouter.get("/", getAllCategories);
