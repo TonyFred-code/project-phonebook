d7d803bb3cf087e5f1c3fb047a3383d47014f17a
Alfred Faith <111481054+TonyFred-code@users.noreply.github.com>
2026-01-30 06:31:27 +0100

    fix: correct category_name validator error msg

--- validators/categoryValidator.js
+++ validators/categoryValidator.js
@@ -8,7 +8,7 @@ function categoryValidationRules() {
       .withMessage("Category name is required")
       .isAlphanumeric("en-US", { ignore: "_-" })
       .withMessage(
-        "Username can only contain letters, numbers, underscores and hyphens"
+        "Category name can only contain letters, numbers, underscores and hyphens"
       )
       .isLength({ min: 1, max: 100 })
       .withMessage("Category name must be between 1 and 100 characters"),
